fa14ff19f27cef5724fbab169da7c2f2d27c314d
SivanGit <9527278+sivangit@user.noreply.gitee.com>
2023-03-13 11:06:31 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    autoplay:true,
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
